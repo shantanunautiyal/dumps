e1ad84f01e9448ceca5aed50a84e9fba30a01b13
SamarV-121 <samarvispute121@gmail.com>
2021-01-21 11:31:38 +0000

    Add full_oppo6785-user-10-QP1A.190711.020-0b36b6c4f9b3bf3b-release-keys

--- vendor/bin/oppo-install-recovery.sh
+++ vendor/bin/oppo-install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-if ! applypatch --check EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:cfd3ca989588c785639e78b95d85cddc972559d9; then
+if ! applypatch --check EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:3e372a33a8761c19a2cd7a20fc963676401b1645; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/platform/bootdevice/by-name/boot:33554432:a5d94e67811f55e05be173b432cf556fea44cef8 \
-          --target EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:cfd3ca989588c785639e78b95d85cddc972559d9 && \
+          --source EMMC:/dev/block/platform/bootdevice/by-name/boot:33554432:aca648944d5f5b593fee519a9736a300953d5c66 \
+          --target EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:3e372a33a8761c19a2cd7a20fc963676401b1645 && \
       log -t recovery "Installing new oppo recovery image: succeeded" && \
       setprop ro.recovery.updated true || \
       log -t recovery "Installing new oppo recovery image: failed" && \
